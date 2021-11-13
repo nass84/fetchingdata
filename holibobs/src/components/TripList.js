6d9061c638ce6a280cd938c095fa6bf0b84a1a22
Becki Floyd <beckifloyd@gmail.com>
2021-11-13 11:46:03 +0000

    Filtering information from json server

---
+++ holibobs/src/components/TripList.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useCallback } from "react";
+
+// Styles
+import "./TripList.css";
+
+export default function TripList() {
+  //fetch data from the server
+  const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setTrips(json);
+  }, [url]);
+
+  useEffect(() => {
+    fetchTrips();
+  }, [fetchTrips]);
+
+  console.log(trips);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="filters">
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+      </div>
+    </div>
+  );
+}
